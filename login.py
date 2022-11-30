de453376db491a57463fb43eca72e82c1757f8b5
yubinax <info@yubinax.com>
2022-11-30 10:41:13 +0000

    login file scritp completed

--- login.py
+++ login.py
@@ -1,3 +1,12 @@
 #This is a login script written in python programming language
 #Author Yubinax Solutions 
 
+echo "Are old are you?"
+read old
+sleep 2
+if [ $old >= 18 ]
+then
+echo "Welcome to adult education class"
+else
+echo "You are not of age to join this class"
+fi
