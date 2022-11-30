eea00594c3cbf505690a96ef59d0d253ec245f08
Yubinax Solutions <118810054+yubinax@users.noreply.github.com>
2022-11-30 11:49:45 +0100

    login file updated to accommodate older citizens

--- login.py
+++ login.py
@@ -4,9 +4,11 @@
 echo "Are old are you?"
 read old
 sleep 2
-if [ $old >= 18 ]
+if [ $old >= 30 ]
 then
 echo "Welcome to adult education class"
 else
 echo "You are not of age to join this class"
 fi
+#this script is only for adult basic education
+#Use this to check the age of interested student 
